fc5f28f4bd7a8830627f5760130af6e7681f0f8f
JoCoKo <jocoko@ya.ru>
2020-01-18 00:47:05 +0300

    init

---
+++ gateway/src/main/java/laba/gateway/BeanConfig.java
@@ -0,0 +1,26 @@
+package laba.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/warehouse/**")
+                        .uri("lb://Warehouse/")
+                        .id("Warehouse"))
+                .route(r -> r.path("/order/**")
+                        .uri("lb://Order/")
+                        .id("Order"))
+                .route(r -> r.path("/payment/**")
+                        .uri("lb://Payment/")
+                        .id("Payment"))
+                .build();
+    }
+
+}
